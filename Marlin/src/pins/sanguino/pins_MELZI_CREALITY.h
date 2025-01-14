thinkyhead/Marlin

♻️ Migrate MINIPANEL

Marlin/src/pins/sanguino/pins_MELZI_CREALITY.h
@@ -59,9 +59,9 @@
 //
 // LCD / Controller
 //
-#if ANY(CR10_STOCKDISPLAY, ENDER2_STOCKDISPLAY, MKS_MINI_12864)
+#if EITHER(CR10_STOCKDISPLAY, MINIPANEL)
   // Migrated to pins/lcd
-  #if EITHER(ENDER2_STOCKDISPLAY, MKS_MINI_12864)
+  #if ENABLED(MINIPANEL)
     #define ADAPTER_MELZI_CREALITY
   #endif
   #define LCD_SDSS                            31  // Controller's SD card

MarlinFirmware/Update-default-Junction-Deviation-numbers-to-match-Classic-Jerk-values

Marlin/src/pins/sanguino/pins_MELZI_CREALITY.h
@@ -58,9 +58,9 @@
 //
 // LCD / Controller
 //
-#if ANY(CR10_STOCKDISPLAY, ENDER2_STOCKDISPLAY, MKS_MINI_12864)
+#if ANY(CR10_STOCKDISPLAY, MINIPANEL)
   // Migrated to pins/lcd
-  #if ANY(ENDER2_STOCKDISPLAY, MKS_MINI_12864)
+  #if ENABLED(MINIPANEL)
     #define ADAPTER_MELZI_CREALITY
   #endif
   #define LCD_SDSS                            31  // Controller's SD card