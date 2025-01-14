thinkyhead/Marlin

♻️ Migrate MKS_MINI_12864

Marlin/src/pins/stm32f1/pins_BTT_SKR_E3_DIP.h
@@ -180,7 +180,7 @@
 
   // Migrated to pins/lcd
 
-#elif ENABLED(ENDER2_STOCKDISPLAY)
+#elif EITHER(ENDER2_STOCKDISPLAY, MKS_MINI_12864)
 
   // Migrated to pins/lcd
   #define FORCE_SOFT_SPI
@@ -200,30 +200,6 @@
     #define LCD_PINS_D6              EXP1_03_PIN
     #define LCD_PINS_D7              EXP1_01_PIN
     #define ADC_KEYPAD_PIN                  PA1   // Repurpose servo pin for ADC - CONNECTING TO 5V WILL DAMAGE THE BOARD!
-
-  #elif ENABLED(MKS_MINI_12864)
-
-    /** Creality Ender-2 display pinout
-     *                   ------
-     *   (SCK)     PA15 | 1  2 | PB6 (BTN_ENC)
-     *   (BTN_EN1) PA9  | 3  4 | RESET
-     *   (BTN_EN2) PA10   5  6 | PB9 (LCD_A0)
-     *   (LCD_RS)  PB8  | 7  8 | PB7 (MOSI)
-     *              GND | 9 10 | 5V
-     *                   ------
-     *                    EXP1
-     */
-
-    #define BTN_ENC                  EXP1_02_PIN
-    #define BTN_EN1                  EXP1_03_PIN
-    #define BTN_EN2                  EXP1_05_PIN
-
-    #define DOGLCD_CS                EXP1_07_PIN
-    #define DOGLCD_A0                EXP1_06_PIN
-    #define DOGLCD_SCK               EXP1_01_PIN
-    #define DOGLCD_MOSI              EXP1_08_PIN
-    #define FORCE_SOFT_SPI
-    #define LCD_BACKLIGHT_PIN               -1
 
   #elif ENABLED(FYSETC_MINI_12864_2_1)
 

Marlin/src/pins/stm32f1/pins_MKS_ROBIN_PRO.h
@@ -344,9 +344,10 @@
     #define TFTGLCD_CS                      PG5
   #endif
 
-#elif ENABLED(ENDER2_STOCKDISPLAY)
+#elif EITHER(ENDER2_STOCKDISPLAY, MKS_MINI_12864)
 
   // Migrated to pins/lcd
+  // MKS MINI12864 and MKS LCD12864B. If using MKS LCD12864A (Need to remove RPK2 resistor)
 
 #elif HAS_WIRED_LCD
 
@@ -359,30 +360,14 @@
   #define LCD_PINS_EN                EXP1_03_PIN
   #define LCD_PINS_RS                EXP1_04_PIN
 
-  // MKS MINI12864 and MKS LCD12864B. If using MKS LCD12864A (Need to remove RPK2 resistor)
-  #if ENABLED(MKS_MINI_12864)
-
-    #define LCD_BACKLIGHT_PIN               -1
-    #define LCD_RESET_PIN                   -1
-    #define DOGLCD_A0                EXP1_07_PIN
-    #define DOGLCD_CS                EXP1_06_PIN
-    #define DOGLCD_SCK               EXP2_02_PIN
-    #define DOGLCD_MOSI              EXP2_06_PIN
-
-  #else
-
-    #define LCD_PINS_D4              EXP1_05_PIN
-    #if IS_ULTIPANEL
-      #define LCD_PINS_D5            EXP1_06_PIN
-      #define LCD_PINS_D6            EXP1_07_PIN
-      #define LCD_PINS_D7            EXP1_08_PIN
-
-      #if ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
-        #define BTN_ENC_EN           LCD_PINS_D7  // Detect the presence of the encoder
-      #endif
-
+  #define LCD_PINS_D4                EXP1_05_PIN
+  #if IS_ULTIPANEL
+    #define LCD_PINS_D5              EXP1_06_PIN
+    #define LCD_PINS_D6              EXP1_07_PIN
+    #define LCD_PINS_D7              EXP1_08_PIN
+    #if ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
+      #define BTN_ENC_EN             LCD_PINS_D7  // Detect the presence of the encoder
     #endif
-
   #endif
 
 #endif // HAS_WIRED_LCD

Marlin/src/pins/stm32f4/pins_BTT_SKR_MINI_E3_V3_0_1.h
@@ -174,7 +174,7 @@
 
   // Migrated to pins/lcd
 
-#elif ENABLED(ENDER2_STOCKDISPLAY)
+#elif EITHER(ENDER2_STOCKDISPLAY, MKS_MINI_12864)
 
   // Migrated to pins/lcd
   #define ADAPTER_BTT_DUAL
@@ -216,20 +216,6 @@
     #define LCD_PINS_D6              EXP1_03_PIN
     #define LCD_PINS_D7              EXP1_01_PIN
     #define ADC_KEYPAD_PIN                  PA1   // Repurpose servo pin for ADC - CONNECTING TO 5V WILL DAMAGE THE BOARD!
-
-  #elif ENABLED(MKS_MINI_12864)
-
-    #define BTN_ENC                  EXP1_02_PIN
-    #define BTN_EN1                  EXP1_03_PIN
-    #define BTN_EN2                  EXP1_05_PIN
-
-    #define DOGLCD_CS                EXP1_07_PIN
-    #define DOGLCD_A0                EXP1_06_PIN
-    #define DOGLCD_SCK               EXP1_01_PIN
-    #define DOGLCD_MOSI              EXP1_08_PIN
-
-    #define FORCE_SOFT_SPI
-    #define LCD_BACKLIGHT_PIN               -1
 
   #elif IS_TFTGLCD_PANEL
 

Marlin/src/pins/stm32f4/pins_BTT_SKR_V2_0_common.h
@@ -445,6 +445,11 @@
 
   // Migrated to pins/lcd
 
+#elif ENABLED(MKS_MINI_12864)
+
+  // Migrated to pins/lcd
+  #define BEEPER_PIN                 EXP1_01_PIN
+
 #elif IS_TFTGLCD_PANEL
 
   #if ENABLED(TFTGLCD_PANEL_SPI)
@@ -453,57 +458,41 @@
 
 #elif HAS_WIRED_LCD
 
-  #define BEEPER_PIN                 EXP1_01_PIN
-  #define BTN_ENC                    EXP1_02_PIN
+  #define LCD_PINS_RS                EXP1_04_PIN
 
-  #if ENABLED(MKS_MINI_12864)
+  #define BTN_EN1                    EXP2_03_PIN
+  #define BTN_EN2                    EXP2_05_PIN
 
-    #define DOGLCD_A0                EXP1_07_PIN
-    #define DOGLCD_CS                EXP1_06_PIN
-    #define BTN_EN1                  EXP2_03_PIN
-    #define BTN_EN2                  EXP2_05_PIN
+  #define LCD_PINS_EN                EXP1_03_PIN
+  #define LCD_PINS_D4                EXP1_05_PIN
 
-  #else
-
-    #define LCD_PINS_RS              EXP1_04_PIN
-
-    #define BTN_EN1                  EXP2_03_PIN
-    #define BTN_EN2                  EXP2_05_PIN
-
-    #define LCD_PINS_EN              EXP1_03_PIN
-    #define LCD_PINS_D4              EXP1_05_PIN
-
-    #if ENABLED(FYSETC_MINI_12864)
-      #define DOGLCD_CS              EXP1_03_PIN
-      #define DOGLCD_A0              EXP1_04_PIN
-      //#define LCD_BACKLIGHT_PIN           -1
-      #define LCD_RESET_PIN          EXP1_05_PIN  // Must be high or open for LCD to operate normally.
-      #if EITHER(FYSETC_MINI_12864_1_2, FYSETC_MINI_12864_2_0)
-        #ifndef RGB_LED_R_PIN
-          #define RGB_LED_R_PIN      EXP1_06_PIN
-        #endif
-        #ifndef RGB_LED_G_PIN
-          #define RGB_LED_G_PIN      EXP1_07_PIN
-        #endif
-        #ifndef RGB_LED_B_PIN
-          #define RGB_LED_B_PIN      EXP1_08_PIN
-        #endif
-      #elif ENABLED(FYSETC_MINI_12864_2_1)
-        #define NEOPIXEL_PIN         EXP1_06_PIN
+  #if ENABLED(FYSETC_MINI_12864)
+    #define DOGLCD_CS                EXP1_03_PIN
+    #define DOGLCD_A0                EXP1_04_PIN
+    //#define LCD_BACKLIGHT_PIN             -1
+    #define LCD_RESET_PIN            EXP1_05_PIN  // Must be high or open for LCD to operate normally.
+    #if EITHER(FYSETC_MINI_12864_1_2, FYSETC_MINI_12864_2_0)
+      #ifndef RGB_LED_R_PIN
+        #define RGB_LED_R_PIN        EXP1_06_PIN
       #endif
-    #endif // !FYSETC_MINI_12864
-
-    #if IS_ULTIPANEL
-      #define LCD_PINS_D5            EXP1_06_PIN
-      #define LCD_PINS_D6            EXP1_07_PIN
-      #define LCD_PINS_D7            EXP1_08_PIN
-
-      #if ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
-        #define BTN_ENC_EN           LCD_PINS_D7  // Detect the presence of the encoder
+      #ifndef RGB_LED_G_PIN
+        #define RGB_LED_G_PIN        EXP1_07_PIN
       #endif
-
+      #ifndef RGB_LED_B_PIN
+        #define RGB_LED_B_PIN        EXP1_08_PIN
+      #endif
+    #elif ENABLED(FYSETC_MINI_12864_2_1)
+      #define NEOPIXEL_PIN           EXP1_06_PIN
     #endif
+  #endif // !FYSETC_MINI_12864
 
+  #if IS_ULTIPANEL
+    #define LCD_PINS_D5              EXP1_06_PIN
+    #define LCD_PINS_D6              EXP1_07_PIN
+    #define LCD_PINS_D7              EXP1_08_PIN
+    #if ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
+      #define BTN_ENC_EN             LCD_PINS_D7  // Detect the presence of the encoder
+    #endif
   #endif
 
 #endif // HAS_WIRED_LCD

Marlin/src/pins/stm32g0/pins_BTT_MANTA_E3_EZ_V1_0.h
@@ -241,7 +241,7 @@
 
   // Migrated to pins/lcd
 
-#elif ENABLED(ENDER2_STOCKDISPLAY)
+#elif EITHER(ENDER2_STOCKDISPLAY, MKS_MINI_12864)
 
   // Migrated to pins/lcd
   #define FORCE_SOFT_SPI
@@ -282,19 +282,6 @@
     #define LCD_PINS_D6              EXP1_03_PIN
     #define LCD_PINS_D7              EXP1_01_PIN
     #define ADC_KEYPAD_PIN                  PA7   // Repurpose default SERVO0_PIN for ADC - CONNECTING TO 5V WILL DAMAGE THE BOARD!
-
-  #elif ENABLED(MKS_MINI_12864)
-
-    #define BTN_EN1                  EXP1_03_PIN
-    #define BTN_EN2                  EXP1_05_PIN
-    #define BTN_ENC                  EXP1_02_PIN
-
-    #define DOGLCD_CS                EXP1_07_PIN
-    #define DOGLCD_A0                EXP1_06_PIN
-    #define DOGLCD_SCK               EXP1_01_PIN
-    #define DOGLCD_MOSI              EXP1_08_PIN
-    #define FORCE_SOFT_SPI
-    #define LCD_BACKLIGHT_PIN               -1
 
   #else
 

Marlin/src/pins/stm32g0/pins_BTT_SKR_MINI_E3_V3_0.h
@@ -166,7 +166,7 @@
 
   // Migrated to pins/lcd
 
-#elif ENABLED(ENDER2_STOCKDISPLAY)
+#elif EITHER(ENDER2_STOCKDISPLAY, MKS_MINI_12864)
 
   // Migrated to pins/lcd
   #define ADAPTER_BTT_DUAL
@@ -252,20 +252,6 @@
       #define LCD_PINS_D6            EXP1_03_PIN
       #define LCD_PINS_D7            EXP1_01_PIN
       #define ADC_KEYPAD_PIN                PA1   // Repurpose servo pin for ADC - CONNECTING TO 5V WILL DAMAGE THE BOARD!
-
-    #elif ENABLED(MKS_MINI_12864)
-
-      #define BTN_ENC                EXP1_02_PIN
-      #define BTN_EN1                EXP1_03_PIN
-      #define BTN_EN2                EXP1_05_PIN
-
-      #define DOGLCD_CS              EXP1_07_PIN
-      #define DOGLCD_A0              EXP1_06_PIN
-      #define DOGLCD_SCK             EXP1_01_PIN
-      #define DOGLCD_MOSI            EXP1_08_PIN
-
-      #define FORCE_SOFT_SPI
-      #define LCD_BACKLIGHT_PIN             -1
 
     #elif IS_TFTGLCD_PANEL
 

MarlinFirmware/Update-default-Junction-Deviation-numbers-to-match-Classic-Jerk-values

Marlin/src/pins/stm32f1/pins_BTT_SKR_E3_DIP.h
@@ -193,7 +193,7 @@
 
   // Migrated to pins/lcd
 
-#elif ENABLED(ENDER2_STOCKDISPLAY)
+#elif ANY(ENDER2_STOCKDISPLAY, MKS_MINI_12864)
 
   // Migrated to pins/lcd
   #define FORCE_SOFT_SPI
@@ -211,30 +211,6 @@
     #define LCD_PINS_D6              EXP1_03_PIN
     #define LCD_PINS_D7              EXP1_01_PIN
     #define ADC_KEYPAD_PIN                  PA1   // Repurpose servo pin for ADC - CONNECTING TO 5V WILL DAMAGE THE BOARD!
-
-  #elif ENABLED(MKS_MINI_12864)
-
-    /** Creality Ender-2 display pinout
-     *                   ------
-     *   (SCK)     PA15 | 1  2 | PB6 (BTN_ENC)
-     *   (BTN_EN1) PA9  | 3  4 | RESET
-     *   (BTN_EN2) PA10   5  6 | PB9 (LCD_A0)
-     *   (LCD_RS)  PB8  | 7  8 | PB7 (MOSI)
-     *              GND | 9 10 | 5V
-     *                   ------
-     *                    EXP1
-     */
-
-    #define BTN_ENC                  EXP1_02_PIN
-    #define BTN_EN1                  EXP1_03_PIN
-    #define BTN_EN2                  EXP1_05_PIN
-
-    #define DOGLCD_CS                EXP1_07_PIN
-    #define DOGLCD_A0                EXP1_06_PIN
-    #define DOGLCD_SCK               EXP1_01_PIN
-    #define DOGLCD_MOSI              EXP1_08_PIN
-    #define FORCE_SOFT_SPI
-    #define LCD_BACKLIGHT_PIN               -1
 
   #elif ENABLED(FYSETC_MINI_12864_2_1)
 

Marlin/src/pins/stm32f1/pins_MKS_ROBIN_PRO.h
@@ -365,9 +365,10 @@
     #define TFTGLCD_CS                      PG5
   #endif
 
-#elif ENABLED(ENDER2_STOCKDISPLAY)
+#elif ANY(ENDER2_STOCKDISPLAY, MKS_MINI_12864)
 
   // Migrated to pins/lcd
+  // MKS MINI12864 and MKS LCD12864B. If using MKS LCD12864A (Need to remove RPK2 resistor)
 
 #elif HAS_WIRED_LCD
 
@@ -380,30 +381,14 @@
   #define LCD_PINS_EN                EXP1_03_PIN
   #define LCD_PINS_RS                EXP1_04_PIN
 
-  // MKS MINI12864 and MKS LCD12864B. If using MKS LCD12864A (Need to remove RPK2 resistor)
-  #if ENABLED(MKS_MINI_12864)
-
-    #define LCD_BACKLIGHT_PIN               -1
-    #define LCD_RESET_PIN                   -1
-    #define DOGLCD_A0                EXP1_07_PIN
-    #define DOGLCD_CS                EXP1_06_PIN
-    #define DOGLCD_SCK               EXP2_02_PIN
-    #define DOGLCD_MOSI              EXP2_06_PIN
-
-  #else // !ENDER2_STOCKDISPLAY && !MKS_MINI_12864
-
-    #define LCD_PINS_D4              EXP1_05_PIN
-    #if IS_ULTIPANEL
-      #define LCD_PINS_D5            EXP1_06_PIN
-      #define LCD_PINS_D6            EXP1_07_PIN
-      #define LCD_PINS_D7            EXP1_08_PIN
-
-      #if ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
-        #define BTN_ENC_EN           LCD_PINS_D7  // Detect the presence of the encoder
-      #endif
-
+  #define LCD_PINS_D4                EXP1_05_PIN
+  #if IS_ULTIPANEL
+    #define LCD_PINS_D5              EXP1_06_PIN
+    #define LCD_PINS_D6              EXP1_07_PIN
+    #define LCD_PINS_D7              EXP1_08_PIN
+    #if ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
+      #define BTN_ENC_EN             LCD_PINS_D7  // Detect the presence of the encoder
     #endif
-
   #endif
 
 #endif // HAS_WIRED_LCD

Marlin/src/pins/stm32f4/pins_BTT_SKR_MINI_E3_V3_0_1.h
@@ -185,7 +185,7 @@
 
   // Migrated to pins/lcd
 
-#elif ENABLED(ENDER2_STOCKDISPLAY)
+#elif ANY(ENDER2_STOCKDISPLAY, MKS_MINI_12864)
 
   // Migrated to pins/lcd
   #define ADAPTER_BTT_DUAL
@@ -225,20 +225,6 @@
     #define LCD_PINS_D6              EXP1_03_PIN
     #define LCD_PINS_D7              EXP1_01_PIN
     #define ADC_KEYPAD_PIN                  PA1   // Repurpose servo pin for ADC - CONNECTING TO 5V WILL DAMAGE THE BOARD!
-
-  #elif ENABLED(MKS_MINI_12864)
-
-    #define BTN_ENC                  EXP1_02_PIN
-    #define BTN_EN1                  EXP1_03_PIN
-    #define BTN_EN2                  EXP1_05_PIN
-
-    #define DOGLCD_CS                EXP1_07_PIN
-    #define DOGLCD_A0                EXP1_06_PIN
-    #define DOGLCD_SCK               EXP1_01_PIN
-    #define DOGLCD_MOSI              EXP1_08_PIN
-
-    #define FORCE_SOFT_SPI
-    #define LCD_BACKLIGHT_PIN               -1
 
   #elif IS_TFTGLCD_PANEL
 

Marlin/src/pins/stm32f4/pins_BTT_SKR_V2_0_common.h
@@ -431,6 +431,11 @@
 
   // Migrated to pins/lcd
 
+#elif ENABLED(MKS_MINI_12864)
+
+  // Migrated to pins/lcd
+  #define BEEPER_PIN                 EXP1_01_PIN
+
 #elif IS_TFTGLCD_PANEL
 
   #if ENABLED(TFTGLCD_PANEL_SPI)
@@ -439,57 +444,41 @@
 
 #elif HAS_WIRED_LCD
 
-  #define BEEPER_PIN                 EXP1_01_PIN
-  #define BTN_ENC                    EXP1_02_PIN
+  #define LCD_PINS_RS                EXP1_04_PIN
 
-  #if ENABLED(MKS_MINI_12864)
+  #define BTN_EN1                    EXP2_03_PIN
+  #define BTN_EN2                    EXP2_05_PIN
 
-    #define DOGLCD_A0                EXP1_07_PIN
-    #define DOGLCD_CS                EXP1_06_PIN
-    #define BTN_EN1                  EXP2_03_PIN
-    #define BTN_EN2                  EXP2_05_PIN
+  #define LCD_PINS_EN                EXP1_03_PIN
+  #define LCD_PINS_D4                EXP1_05_PIN
 
-  #else
-
-    #define LCD_PINS_RS              EXP1_04_PIN
-
-    #define BTN_EN1                  EXP2_03_PIN
-    #define BTN_EN2                  EXP2_05_PIN
-
-    #define LCD_PINS_EN              EXP1_03_PIN
-    #define LCD_PINS_D4              EXP1_05_PIN
-
-    #if ENABLED(FYSETC_MINI_12864)
-      #define DOGLCD_CS              EXP1_03_PIN
-      #define DOGLCD_A0              EXP1_04_PIN
-      //#define LCD_BACKLIGHT_PIN           -1
-      #define LCD_RESET_PIN          EXP1_05_PIN  // Must be high or open for LCD to operate normally.
-      #if ANY(FYSETC_MINI_12864_1_2, FYSETC_MINI_12864_2_0)
-        #ifndef RGB_LED_R_PIN
-          #define RGB_LED_R_PIN      EXP1_06_PIN
-        #endif
-        #ifndef RGB_LED_G_PIN
-          #define RGB_LED_G_PIN      EXP1_07_PIN
-        #endif
-        #ifndef RGB_LED_B_PIN
-          #define RGB_LED_B_PIN      EXP1_08_PIN
-        #endif
-      #elif ENABLED(FYSETC_MINI_12864_2_1)
-        #define NEOPIXEL_PIN         EXP1_06_PIN
+  #if ENABLED(FYSETC_MINI_12864)
+    #define DOGLCD_CS                EXP1_03_PIN
+    #define DOGLCD_A0                EXP1_04_PIN
+    //#define LCD_BACKLIGHT_PIN             -1
+    #define LCD_RESET_PIN            EXP1_05_PIN  // Must be high or open for LCD to operate normally.
+    #if ANY(FYSETC_MINI_12864_1_2, FYSETC_MINI_12864_2_0)
+      #ifndef RGB_LED_R_PIN
+        #define RGB_LED_R_PIN        EXP1_06_PIN
       #endif
-    #endif // !FYSETC_MINI_12864
-
-    #if IS_ULTIPANEL
-      #define LCD_PINS_D5            EXP1_06_PIN
-      #define LCD_PINS_D6            EXP1_07_PIN
-      #define LCD_PINS_D7            EXP1_08_PIN
-
-      #if ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
-        #define BTN_ENC_EN           LCD_PINS_D7  // Detect the presence of the encoder
+      #ifndef RGB_LED_G_PIN
+        #define RGB_LED_G_PIN        EXP1_07_PIN
       #endif
-
+      #ifndef RGB_LED_B_PIN
+        #define RGB_LED_B_PIN        EXP1_08_PIN
+      #endif
+    #elif ENABLED(FYSETC_MINI_12864_2_1)
+      #define NEOPIXEL_PIN           EXP1_06_PIN
     #endif
+  #endif // !FYSETC_MINI_12864
 
+  #if IS_ULTIPANEL
+    #define LCD_PINS_D5              EXP1_06_PIN
+    #define LCD_PINS_D6              EXP1_07_PIN
+    #define LCD_PINS_D7              EXP1_08_PIN
+    #if ENABLED(REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER)
+      #define BTN_ENC_EN             LCD_PINS_D7  // Detect the presence of the encoder
+    #endif
   #endif
 
 #endif // HAS_WIRED_LCD

Marlin/src/pins/stm32g0/pins_BTT_MANTA_E3_EZ_V1_0.h
@@ -230,7 +230,7 @@
 
   // Migrated to pins/lcd
 
-#elif ENABLED(ENDER2_STOCKDISPLAY)
+#elif ANY(ENDER2_STOCKDISPLAY, MKS_MINI_12864)
 
   // Migrated to pins/lcd
   #define FORCE_SOFT_SPI
@@ -283,19 +283,6 @@
     #define LCD_PINS_D6              EXP1_03_PIN
     #define LCD_PINS_D7              EXP1_01_PIN
     #define ADC_KEYPAD_PIN                  PA7   // Repurpose default SERVO0_PIN for ADC - CONNECTING TO 5V WILL DAMAGE THE BOARD!
-
-  #elif ENABLED(MKS_MINI_12864)
-
-    #define BTN_EN1                  EXP1_03_PIN
-    #define BTN_EN2                  EXP1_05_PIN
-    #define BTN_ENC                  EXP1_02_PIN
-
-    #define DOGLCD_CS                EXP1_07_PIN
-    #define DOGLCD_A0                EXP1_06_PIN
-    #define DOGLCD_SCK               EXP1_01_PIN
-    #define DOGLCD_MOSI              EXP1_08_PIN
-    #define FORCE_SOFT_SPI
-    #define LCD_BACKLIGHT_PIN               -1
 
   #else
 

Marlin/src/pins/stm32g0/pins_BTT_SKR_MINI_E3_V3_0.h
@@ -191,7 +191,7 @@
 
   // Migrated to pins/lcd
 
-#elif ENABLED(ENDER2_STOCKDISPLAY)
+#elif ANY(ENDER2_STOCKDISPLAY, MKS_MINI_12864)
 
   // Migrated to pins/lcd
   #define ADAPTER_BTT_DUAL
@@ -273,20 +273,6 @@
       #define LCD_PINS_D6            EXP1_03_PIN
       #define LCD_PINS_D7            EXP1_01_PIN
       #define ADC_KEYPAD_PIN                PA1   // Repurpose servo pin for ADC - CONNECTING TO 5V WILL DAMAGE THE BOARD!
-
-    #elif ENABLED(MKS_MINI_12864)
-
-      #define BTN_ENC                EXP1_02_PIN
-      #define BTN_EN1                EXP1_03_PIN
-      #define BTN_EN2                EXP1_05_PIN
-
-      #define DOGLCD_CS              EXP1_07_PIN
-      #define DOGLCD_A0              EXP1_06_PIN
-      #define DOGLCD_SCK             EXP1_01_PIN
-      #define DOGLCD_MOSI            EXP1_08_PIN
-
-      #define FORCE_SOFT_SPI
-      #define LCD_BACKLIGHT_PIN             -1
 
     #elif IS_TFTGLCD_PANEL
 